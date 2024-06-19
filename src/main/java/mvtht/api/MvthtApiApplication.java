f2ba5ff20ea3e6c79b12fafeb948d775a4f33d41
RGB-Keys <ramongtstudy@gmail.com>
2024-06-19 11:53:10 -0300

    Starting project

---
+++ src/main/java/mvtht/api/MvthtApiApplication.java
@@ -0,0 +1,13 @@
+package mvtht.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvthtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvthtApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvtht/api/MvthtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mvtht.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvthtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
